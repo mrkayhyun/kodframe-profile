1fd452ca3a10b3f4b03e2d703be2d5d26819da9a
dhkim <mrkayhyun@gmail.com>
2025-04-15 12:19:17 +0900

    first commit

---
+++ config/site.ts
@@ -0,0 +1,49 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "kodframe home",
+  description: "",
+  navItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Team",
+      href: "/team",
+    },
+    {
+      label: "Calendar",
+      href: "/calendar",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/mrkayhyun",
+  },
+};
